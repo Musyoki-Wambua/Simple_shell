ef943c10be5150e6c9fd86a0ede0f62729414cec
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-17 15:43:19 +0300

    Initial commit

---
+++ shell.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/***** MACROS *****/
+#define PRINT(c) (write(STDERR_FILENO, c, _strlen(c)))
+#define BUFSIZE 10240
+#define DELIMITER " \t\r\n\a"
+
+
+#endif /*HEADER_H*/
