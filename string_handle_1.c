a6d3d2427dc6e3a278744a7a25ca98196c90a1ea
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-10-17 22:21:57 +0300

    Added a function to convert string to a integer

--- string_handle_1.c
+++ string_handle_1.c
@@ -65,3 +65,33 @@ int _strlen(char *s)
 	}
 	return (i);
 }
+
+/**
+ * _atoi - function to convert string to a integer
+ * @s: string input
+ * Return: converted integer
+ */
+int _atoi(char *s)
+{
+	int i = 0, j = 1, k;
+	unsigned int l = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			return (2);
+		}
+		k = s[i] - '0';
+
+		if (l > 0 && !(k >= 0 && k <= 9))
+			break;
+
+		if (k >= 0 && k <= 9)
+			l = l * 10 + k;
+
+		i++;
+	}
+	l *= j;
+	return (l);
+}
